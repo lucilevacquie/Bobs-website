08a7b28abc6a8329c590b48cb3d1c1dce683327d
Lucile Vacquie <lucilevacquie@luciles-mbp.mynet>
2021-01-27 21:34:05 +0000

    link and styled signature

--- src/components/signature.js
+++ src/components/signature.js
@@ -5,14 +5,32 @@ const Container = styled.div`
     position: absolute;
     display: flex;
     width: 100%;
-    bottom: 0;
+    justify-content: center;
+    bottom: 10px;
     font-family: "Roboto", sans-serif;
     color: white;
     z-index: 2;
+    text-shadow: 0px 0px 6px black;
+`;
+
+const Link = styled.a`
+    text-decoration: none;
+    color: white;
+    font-weight: bold;
 `;
 
 const Signature = () => {
-    return <Container>Created and designed by Lucile Vacquie</Container>;
+    return (
+        <Container>
+            Created and designed by
+            <Link
+                href="https://www.linkedin.com/in/lucile-vacqui%C3%A929/"
+                target="_blank"
+            >
+                &nbsp;Lucile Vacquie
+            </Link>
+        </Container>
+    );
 };
 
 export default Signature;
